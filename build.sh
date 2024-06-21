c22ec637434b4189688ce8d68fcd0b7045c3ce31
James Davidson <james@jamesdavidson.xyz>
2024-06-21 08:45:05 +0100

    Add build instructions

--- build.sh
+++ build.sh
@@ -13,11 +13,16 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+#rpm-ostree install screen
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
+
+# Add Papirus icon theme and userspace driver for Corsair mice
+rpm-ostree install papirus-icon-theme ckb-next
+# Nuke nano from orbit
+rpm-ostree remove default-editor nano-default-editor nano
