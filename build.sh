a2d33850f7569f72df9646ea67949f56e36134af
James Davidson <james@jamesdavidson.xyz>
2024-07-17 06:11:00 +0100

    Remove nano and replace with vim
    Additionally remove some unwanted input packages

--- build.sh
+++ build.sh
@@ -26,5 +26,12 @@ RELEASE="$(rpm -E %fedora)"
 # The Papirus icon theme
 # A userspace driver for Corsair gaming mice
 rpm-ostree install papirus-icon-theme ckb-next
-# Nuke nano from orbit
-rpm-ostree remove default-editor nano-default-editor nano
+
+# Remove (in order):
+# The nano text editor (install nano via homebrew, or to containers as needed)
+# input-* packages (overlay packages; not present in uBlue main)
+rpm-ostree uninstall default-editor nano-default-editor nano input-leap input-remapper
+
+# Export ViM as $EDITOR in /etc/profile.d/
+# Used for system-level root tasks, such as sudoedit or visudo
+# rpm-ostree install vim-default-editor
