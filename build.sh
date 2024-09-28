0661458ab29e6b8ef9084e108c1514ceeac291d8
James Davidson <james@jamesdavidson.xyz>
2024-09-28 22:07:12 +0100

    feat(build.sh)!: remove vim (#11)
    * docs(Containerfile): remove template comments
    
    * fix(xdg-open): re-implement if condition
    
    remove comments
    
    * refactor(build.sh): use dash
    
    * feat(build.sh)!: remove vim
    
    remove comments and unused $RELEASE var
    
    * fix(build): use bash shebang
    
    dash is not available yet

--- build.sh
+++ build.sh
@@ -1,35 +1,8 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
-
-
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-#rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-#systemctl enable podman.socket
-
-# Add (in order):
-# The Papirus icon theme
-# A userspace driver for Corsair gaming mice
-# Debian Almquist SHell (for POSIX compliance and speed)
-# Neovim (for reliability when performing system-level tasks)
 rpm-ostree install papirus-icon-theme ckb-next dash neovim
 systemctl enable ckb-next-daemon.service
 
-# Remove (in order):
-# The nano text editor (install nano via homebrew, or to containers as needed)
-rpm-ostree uninstall default-editor nano-default-editor nano
+rpm-ostree uninstall default-editor nano-default-editor nano vim-data vim-minimal vim-filesystem vim-common vim-enhanced
