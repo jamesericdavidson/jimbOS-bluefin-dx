15d6c4356dca9d6ddefb53db8a93e70556fa9acb
James Davidson <james@jamesdavidson.xyz>
2024-10-05 03:21:22 +0100

    fix(build): restore vim

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -ouex pipefail
 rpm-ostree install papirus-icon-theme ckb-next dash neovim
 systemctl enable ckb-next-daemon.service
 
-rpm-ostree uninstall default-editor nano-default-editor nano vim-data vim-minimal vim-filesystem vim-common vim-enhanced
+rpm-ostree uninstall default-editor nano-default-editor nano
