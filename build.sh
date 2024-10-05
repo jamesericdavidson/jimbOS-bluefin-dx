2419b6bffd9838e5ce48034e4fc621367de7f3f0
James Davidson <james@jamesdavidson.xyz>
2024-10-05 03:21:44 +0100

    chore(build)!: remove neovim
    builds from 2024-10-03 started failing as vim was a dependency, moreover
    neovim is not as useful as an all-round editor (e.g. [n]vimdiff)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-rpm-ostree install papirus-icon-theme ckb-next dash neovim
+rpm-ostree install papirus-icon-theme ckb-next dash
 systemctl enable ckb-next-daemon.service
 
 rpm-ostree uninstall default-editor nano-default-editor nano
