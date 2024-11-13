5d3edb1542109497c454768d952c96b5c5f3aff7
James Davidson <james@jamesdavidson.xyz>
2024-11-13 08:48:18 +0000

    feat: add v4l-utils

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-rpm-ostree install papirus-icon-theme ckb-next dash
+rpm-ostree install papirus-icon-theme ckb-next dash v4l-utils
 systemctl enable ckb-next-daemon.service
 
 rpm-ostree uninstall default-editor nano-default-editor nano
