307529023bbf10be1aaf6f77b9d9e63498bf4e14
James Davidson <james@jamesdavidson.xyz>
2024-11-13 09:41:00 +0000

    feat: add guvcview

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-rpm-ostree install papirus-icon-theme ckb-next dash v4l-utils
+rpm-ostree install papirus-icon-theme ckb-next dash v4l-utils guvcview
 systemctl enable ckb-next-daemon.service
 
 rpm-ostree uninstall default-editor nano-default-editor nano
